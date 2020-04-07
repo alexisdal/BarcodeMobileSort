71bbec5034c723a109c57291031f2a08a317b753
alexis <alexisdal@hotmail.com>
2020-04-07 18:28:34 +0200

    with live camera feed working

--- app/src/main/java/com/example/barcodemobilesort/MainActivity.kt
+++ app/src/main/java/com/example/barcodemobilesort/MainActivity.kt
@@ -6,9 +6,22 @@ import android.os.Bundle
 import android.widget.Button
 import kotlin.random.Random
 import android.util.Log
-
+import android.Manifest
+import android.content.pm.PackageManager
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CAMERA_PERMISSION = 10
+    }
+    private lateinit var textureView: TextureView
 
     lateinit var sortButton: Button
 
@@ -16,6 +29,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textureView = findViewById(R.id.texture_view)
+        // Request camera permissions
+        if (isCameraPermissionGranted()) {
+            textureView.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+        }
 
         sortButton = findViewById<Button>(R.id.sortButton)
         sortButton.setOnClickListener {
@@ -41,8 +62,48 @@ class MainActivity : AppCompatActivity() {
             val intent: Intent = Intent(applicationContext, Put2ShelfActivity::class.java)
             intent.putExtra(Put2ShelfActivity.EAN_MSG, eanToSend)
             startActivity(intent)
+
         }
 
+    }
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder()
+            // We want to show input from back camera of the device
+            .setLensFacing(CameraX.LensFacing.BACK)
+            .build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener { previewOutput ->
+            val parent = textureView.parent as ViewGroup //https://stackoverflow.com/questions/56064248/android-camerax-doesnt-show-anything
+            parent.removeView(textureView)
+            textureView.surfaceTexture = previewOutput.surfaceTexture
+            parent.addView(textureView, 0)
+            //updateTransform()
+        }
+
+        val imageAnalysisConfig = ImageAnalysisConfig.Builder()
+            .build()
+        val imageAnalysis = ImageAnalysis(imageAnalysisConfig)
+
+
+        CameraX.bindToLifecycle(this as LifecycleOwner, preview, imageAnalysis)
+    }
+
+    private fun isCameraPermissionGranted(): Boolean {
+        val selfPermission = ContextCompat.checkSelfPermission(baseContext, Manifest.permission.CAMERA)
+        return selfPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (isCameraPermissionGranted()) {
+                textureView.post { startCamera() }
+            } else {
+                Toast.makeText(this, "Camera permission is required.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 
 
